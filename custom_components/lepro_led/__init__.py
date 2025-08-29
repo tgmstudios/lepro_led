dd5bc6e3b18ec186ca1b769c63a8eb0b67afb295
Sanji78 <166992508+Sanji78@users.noreply.github.com>
2025-08-29 18:55:27 +0200

    Update __init__.py

--- custom_components/lepro_led/__init__.py
+++ custom_components/lepro_led/__init__.py
@@ -1,7 +1,7 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
-
+from homeassistant.helpers import config_validation as cv
 from .const import DOMAIN
 
 # Config entry only (no YAML)
