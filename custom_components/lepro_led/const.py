da95907e1a4d9b0c6b4ec2b90a5a1c7fbb2cf698
Aiden Johnson <65328952+tgmstudios@users.noreply.github.com>
2025-10-23 01:35:40 -0400

    US support

--- custom_components/lepro_led/const.py
+++ custom_components/lepro_led/const.py
@@ -1,8 +1,8 @@
 DOMAIN = "lepro_led"
 
-LOGIN_URL = "https://api-eu-iot.lepro.com/user/login"
-FAMILY_LIST_URL = "https://api-eu-iot.lepro.com/family/list/timestamp/{timestamp}"
-USER_PROFILE_URL = "https://api-eu-iot.lepro.com/user/profile"
-DEVICE_LIST_URL = "https://api-eu-iot.lepro.com/v3/device/list/fid/{fid}/timestamp/{timestamp}"
-SWITCH_API_URL = "https://api-eu-iot.lepro.com/statistic/record"
+LOGIN_URL = "https://api-na-iot.lepro.com/user/login"
+FAMILY_LIST_URL = "https://api-na-iot.lepro.com/family/list/timestamp/{timestamp}"
+USER_PROFILE_URL = "https://api-na-iot.lepro.com/user/profile"
+DEVICE_LIST_URL = "https://api-na-iot.lepro.com/v3/device/list/fid/{fid}/timestamp/{timestamp}"
+SWITCH_API_URL = "https://api-na-iot.lepro.com/statistic/record"
 
